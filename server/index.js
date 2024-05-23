ae83bea010c81dfc6a89b3cd303fdef1608b5237
sudharshana <sudhubalu04@gmail.com>
2024-05-23 19:13:48 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 import router from './router/index.js'
-
-
+import 'dotenv/config'
 import express from "express"
+import mongoose from "mongoose"
 
 const app = express()
 const port=8000
-
-
 
 app.use(express.json())
 app.use(router)
 
 
-  app.get('/',(req,res)=>{
-
-    res.send("hello sudhu")
+app.get('/',(req,res)=>{
+  res.json({
+    name:"sudhu",
+    age:20
+  })
 })
-
-
 
 app.listen(port,()=>{
     console.log("Server is running");
 })
+
+if(process.env.mongodb_url)
+  {
+    try {
+      mongoose.connect(process.env.mongodb_url)
+      console.log("connected");
+    } catch (error) {
+      console.log("not connected",error);
+    }
+
+  }
+  else{
+    console.log("url not found");
+  }
